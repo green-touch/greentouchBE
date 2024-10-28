78b1e7dc587b04359c2d3357b5aad17da97cd584
hann <wjdgks1233@gmail.com>
2024-10-28 18:53:12 +0900

    Init Project

---
+++ src/main/java/hacuna/greentouch/GreentouchApplication.java
@@ -0,0 +1,13 @@
+package hacuna.greentouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreentouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreentouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hacuna/greentouch/GreentouchApplicationTests.java
@@ -0,0 +1,13 @@
+package hacuna.greentouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreentouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
