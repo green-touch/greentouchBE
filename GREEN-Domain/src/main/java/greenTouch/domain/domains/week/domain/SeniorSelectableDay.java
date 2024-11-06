df12c9ad9416093504837b4ec9583b744c586c1f
hann <wjdgks1233@gmail.com>
2024-11-06 20:20:45 +0900

    [GREEN-53] feature/#3 : create SeniorSelecatbleDAy entity

---
+++ GREEN-Domain/src/main/java/greenTouch/domain/domains/week/domain/SeniorSelectableDay.java
@@ -0,0 +1,29 @@
+package greenTouch.domain.domains.week.domain;
+
+import greenTouch.domain.common.model.DayOfWeek;
+import greenTouch.domain.domains.member.domain.Senior;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SeniorSelectableDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "senior_selectable_day_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private SelectableDay selectableDay;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "senior_id")
+    private Senior senior;
+}
